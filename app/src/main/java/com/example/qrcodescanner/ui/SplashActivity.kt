55ee7b8f8fd3c2e808decf0833975227f2a0e908
duongtx <duongtx.t2.1922@gmail.com>
2025-06-27 22:10:24 +0700

    first commit

---
+++ app/src/main/java/com/example/qrcodescanner/ui/SplashActivity.kt
@@ -0,0 +1,102 @@
+package com.example.qrcodescanner.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material3.AlertDialog
+import androidx.core.app.ActivityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.qrcodescanner.MainActivity
+import com.example.qrcodescanner.R
+
+class SplashActivity : AppCompatActivity() {
+    companion object {
+        private const val CAMERA_PERMISSION_REQUEST_CODE = 1001
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        checkForPermission()
+    }
+
+    private fun checkForPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.CAMERA
+            ) == android.content.pm.PackageManager.PERMISSION_GRANTED
+        ) {
+            gotoMainActivity()
+        } else
+            requestThePermission()
+
+    }
+
+    private fun requestThePermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(android.Manifest.permission.CAMERA),
+            CAMERA_PERMISSION_REQUEST_CODE
+        )
+    }
+
+    private fun isUserPermanentlyDenied(): Boolean {
+        return shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA).not()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == android.content.pm.PackageManager.PERMISSION_GRANTED) {
+                gotoMainActivity()
+            } else if (isUserPermanentlyDenied()) {
+               showGoToAppSettingsDialog()
+            }
+            else
+                requestThePermission()
+        }
+    }
+
+    private fun showGoToAppSettingsDialog() {
+        androidx.appcompat.app.AlertDialog.Builder(this)
+            .setTitle("Grant Camera Permission")
+            .setMessage("We need camera permission to scan QR codes. Please go to settings and enable it.")
+            .setPositiveButton("Grant") { dialog, _ ->
+                gotoAppSettings()
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                Toast.makeText(this, "We need camera permission for Start this Application", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            .show()
+    }
+    private fun gotoAppSettings() {
+       var intent = android.content.Intent(android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        intent.data = android.net.Uri.fromParts("package", packageName, null)
+        intent.addCategory(Intent.CATEGORY_DEFAULT)
+        intent.addFlags(android.content.Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+    }
+    private fun gotoMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        checkForPermission()
+    }
+}

a038f6fe44b6127221ff1c607af1b700e9e65298
duongtx <duongtx.t2.1922@gmail.com>
2025-06-27 22:40:37 +0700

    fix permission

--- app/src/main/java/com/example/qrcodescanner/ui/SplashActivity.kt
+++ app/src/main/java/com/example/qrcodescanner/ui/SplashActivity.kt
@@ -1,6 +1,8 @@
 package com.example.qrcodescanner.ui
 
+import android.Manifest
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -14,18 +16,13 @@ import com.example.qrcodescanner.R
 
 class SplashActivity : AppCompatActivity() {
     companion object {
-        private const val CAMERA_PERMISSION_REQUEST_CODE = 1001
+        private const val CAMERA_PERMISSION_REQUEST_CODE = 123
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
         checkForPermission()
     }
 
@@ -50,7 +47,11 @@ class SplashActivity : AppCompatActivity() {
     }
 
     private fun isUserPermanentlyDenied(): Boolean {
-        return shouldShowRequestPermissionRationale(android.Manifest.permission.CAMERA).not()
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            shouldShowRequestPermissionRationale(Manifest.permission.CAMERA).not()
+        } else {
+            return false
+        }
     }
 
     override fun onRequestPermissionsResult(
